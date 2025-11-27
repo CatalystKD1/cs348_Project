e1bd5e0d485a0b6dda66035df08273f01e0d0a01
CatalystKD_ <81048903+CatalystKD1@users.noreply.github.com>
2025-11-27 18:30:39 -0500

    fix query

--- milestone-3/production-SQL/f4-test-production.sql
+++ milestone-3/production-SQL/f4-test-production.sql
@@ -1,3 +1,3 @@
 /* Gets the follower count and artist name for two random artists */
 
-SELECT artist_name, followers FROM Artists ORDER BY rand() LIMIT 50;
+SELECT artist_name, followers FROM Artists ORDER BY rand() LIMIT 2;
